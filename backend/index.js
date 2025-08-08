1da216c30b5602601fd4c218b10c2a87ef30cd9e
iimirzaa <iimirzaaa@gmail.com>
2025-08-08 22:25:28 +0500

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express=require('express')
 const app=express();
 const dotenv=require("dotenv")
 dotenv.config();
+app.use(express.urlencoded({extend:true}));
+app.use(express.json());
 app.listen(3000,(req,res)=>{
     console.log("Server created Successfully!")
 })
